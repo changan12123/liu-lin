72c4763cafbabe4ae9f1440d997a65678cd1ce7f
changan12123 <1401185376@qq.com>
2019-03-17 19:50:58 +0800

    2.16

---
+++ 2.16/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum,multiply,c,d;
+    cout << "please input 2 numbers:" << endl;
+    cin >> a >> b;
+    sum = a + b ;
+    cout<<"sum=\n"<<sum;
+    multiply = a * b;
+    cout<<"multiply=\n"<<multiply;
+    c = a - b;
+    cout<<"c=\n"<<c;
+    d = a / b;
+    cout<<"d=\n"<<d;
+
+    return 0;
+}
+
+
